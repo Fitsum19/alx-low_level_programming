4e57c2213be72c62870a7980af63bb288adf444d
fitsum19 <fitsumawoke5@gmail.com>
2022-12-09 11:28:20 -0800

    yes

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-
 /**
- * main - prints the alphabet in lowercase,
- *
+ *main - prints the alphabet in lowercase,
  *Return: Aways 0 (Sucess)
- *
  */
-
 int main(void)
-
 {
 char ch = 'a';
 while (ch <= 'z')
 {								
 if (ch != 'e' && ch != 'q')
 {
-putchar(ch);										}									   ch++;									}
+putchar(ch);
+}
+ch++;
+}
 putchar('\n');
 return (0);
 }
