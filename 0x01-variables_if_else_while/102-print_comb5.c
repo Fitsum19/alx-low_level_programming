d11e88beda351d771055a5ee4ace38d5814ef9cb
fitsum19 <fitsumawoke5@gmail.com>
2022-12-09 11:05:36 -0800

    yes

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,8 @@ if (i < j)
 putchar((i / 10) + 48);
 putchar((i % 10) + 48);
 putchar(' ');
-putchar((j / 10) + 48);								putchar((j % 10) + 48);
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
 if (i != 98 || j != 99)
 {
 putchar(',');
