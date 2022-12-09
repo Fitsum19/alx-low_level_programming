c4079135dcdcafc30395c39e0f51b5a9422d9122
fitsum19 <fitsumawoke5@gmail.com>
2022-12-09 11:11:31 -0800

    yes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,14 +5,17 @@
  */
 
 int main(void)
-
 {
 int n;
 for (n = 48; n < 58; n++)
 {
-putchar(n);										if (n != 57)								{
-putchar(',');																				putchar(' ');
-}										}
+putchar(n);
+if (n != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
 putchar('\n');
 return (0);
 }
