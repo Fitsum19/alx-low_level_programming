404ca2b8ac8b275e93a7ba1b8b45a225134fc70b
fitsum19 <fitsumawoke5@gmail.com>
2022-12-09 10:36:01 -0800

    yes

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,7 +10,11 @@ int main(void)
 int n, m, l;
 for (n = 48; n < 58; n++)
 {
-for (m = 49; m < 58; m++)							{
+
+for (m = 49; m < 58; m++)							
+
+{
+
 for (l = 50; l < 58; l++)
 {
 if (l > m && m > n)
