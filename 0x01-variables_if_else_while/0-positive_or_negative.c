b16fe394dd26b84dee22b73d14e9c2b6f9e1b120
fitsum19 <fitsumawoke5@gmail.com>
2022-12-09 05:29:11 -0800

    yes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/**
+ *main - main block 
+ *Description - get a random number and print number
+ *Return 0
+ */
 int main(void)
 {
 int n;
