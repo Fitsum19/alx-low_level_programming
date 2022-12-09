4e07a38dc938c81939d0292b3c30b1f486d81894
fitsum19 <fitsumawoke5@gmail.com>
2022-12-09 11:25:17 -0800

    yes

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-
 /**
- *
  *main - prints all single digit numbers of base
- *
- * 10 starting from 0, followed by a new line
+ *Description- 10 starting from 0, followed by a new line
  * Return: Always 0 (Success)
  */
 
