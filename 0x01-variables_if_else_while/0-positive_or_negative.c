c63334ac0fad526d1ba4a95a736127c09343daa6
fitsum19 <fitsumawoke5@gmail.com>
2022-12-09 05:14:31 -0800

    yes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,13 +13,13 @@ if (n > 0)
 {
 printf("%i is positive\n", n);
 }
-else if (n = 0)
+else if (n < 0)
 {
-printf("%i is zero\n", n);
+printf("%i is negative\n", n);
 }
 else
 {
-printf("%i is negative\n", n);
+printf("%i is zero\n", n);
 }
 return (0);
 }
