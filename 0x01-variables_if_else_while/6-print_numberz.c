312cd26a1adb6c720a7919a30400dfdc52e9f31d
fitsum19 <fitsumawoke5@gmail.com>
2022-12-09 11:22:10 -0800

    yes

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,9 @@
-#include <stdio.h>
-
+#include <stdio.h
 /**
- *
  *main - prints all single digit numbers of base 10 starting from 0,
  * followed by a new line, using putchar
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 int n;
