9bd34b99d80d2c664457e955b8bd2eca533c9fbe
fitsum19 <fitsumawoke5@gmail.com>
2022-12-13 09:27:21 -0800

    second

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - funtion print alphabet 10 times
+ *
+ *Return - X10 a-z
+ */
+void print_alphabet_x10(void)
+{
+int i, m = 0;
+while (m < 10)
+{
+  for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+m++;
+_putchar('\n');
+}
+}
