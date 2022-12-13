6bd9a5934b9fbb559e074cc151d0e73e7d226f94
fitsum19 <fitsumawoke5@gmail.com>
2022-12-13 06:08:21 -0800

    second try

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,15 @@
  *
  *Return: Always 0 (Success)
  */
-
 int main(void)
 {
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
-  return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
