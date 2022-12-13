fe4e0ad83632f4cf201f5db20b7f113f0898fb25
fitsum19 <fitsumawoke5@gmail.com>
2022-12-13 06:04:47 -0800

    first try

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *main: Entry Point 
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+  return (0);
+}
