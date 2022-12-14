c25d495ec2bf8a9abe5b63e1648d40855b35a635
fitsum19 <fitsumawoke5@gmail.com>
2022-12-14 06:50:38 -0800

    yes

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-int main(void);
-void postive_or_negative(int i);
+void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int day, int month);
 void print_remaining_days(int month, int day, int year);
