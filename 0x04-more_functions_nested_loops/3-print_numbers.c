260586514b1a6e6b91aff464ce28c848f3086c11
fitsum19 <fitsumawoke5@gmail.com>
2022-12-15 06:10:51 -0800

    uff

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- *print_numbers- write number from 0 to 9
- *Return: 0
- */
+*print_numbers- write number from 0 to 9
+*Return: 0
+*/
 void print_numbers(void)
 {
 char c;
 
-for (c >= '0'; c <= '9'; c++)
+for (c = '0'; c <= '9'; c++)
 {
 _putchar(c);
 }
