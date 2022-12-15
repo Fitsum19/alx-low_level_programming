2ecb4d458b8d6a9cb90dc3d9f8b2a41b7222ebfe
fitsum19 <fitsumawoke5@gmail.com>
2022-12-15 10:15:45 -0800

    yes

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-*main - tests function that prints if integer is positive or negative
-*Return: 0
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 int main(void)
 {
@@ -10,3 +10,4 @@ i = 98;
 positive_or_negative(i);
 return (0);
 }
+
