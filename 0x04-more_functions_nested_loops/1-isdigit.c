8c3064c64477c973310ac18949a232da2e8fb075
fitsum19 <fitsumawoke5@gmail.com>
2022-12-15 05:47:19 -0800

    yes

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_isdigit - print value from 0 upto 9
+ * @c: check the number
+ *Return: 1 or 0
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+}
