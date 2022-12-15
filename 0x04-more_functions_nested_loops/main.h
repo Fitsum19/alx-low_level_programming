b903a494c209ee4f67b52c6b633264f1af77c635
fitsum19 <fitsumawoke5@gmail.com>
2022-12-15 05:32:47 -0800

    yes

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,4 +12,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif /* MAIN_H */
+#endif
