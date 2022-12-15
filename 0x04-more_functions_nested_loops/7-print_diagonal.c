84e300754ce58e30bf7609d9460c5a0cf9bc955a
fitsum19 <fitsumawoke5@gmail.com>
2022-12-15 07:08:14 -0800

    yes

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_diagonal- print a diagonal line
+ *@n: number of times
+ *Return: 0
+ */
+void print_diagonal(int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+_putchar('/');
+}
+_putchar('\n');
+}
