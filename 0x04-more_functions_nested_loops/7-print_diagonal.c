fd123e294d46a614488b7ad53f785829501b1f27
fitsum19 <fitsumawoke5@gmail.com>
2022-12-15 07:16:17 -0800

    yes

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,11 +6,17 @@
  */
 void print_diagonal(int n)
 {
-int i;
+int i, j;
 
+if (n <= 0)
+_putchar('\n');
 for (i = 0; i < n; i++)
 {
-_putchar('/');
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
 }
+_putchar('\\');
 _putchar('\n');
+}
 }
