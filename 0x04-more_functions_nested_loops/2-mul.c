51274063125d595ba3457e93c7d7908a7fa7b941
fitsum19 <fitsumawoke5@gmail.com>
2022-12-15 06:01:53 -0800

    yes

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *mul- multiplication between two numbers
+ *@a: First Number
+ *@b: Second Number
+ *Return: Result of Multipilication
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
