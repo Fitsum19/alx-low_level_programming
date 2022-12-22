a6a268b5b7112dcf7264c34e44e466e3d1ae96d7
fitsum19 <fitsumawoke5@gmail.com>
2022-12-22 10:31:12 -0800

    yes

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _strcmp - compare values
+* @s1: value
+* @s2: value
+* Return: void
+*/
+int _strcmp(char *s1, char *s2)
+{
+int i;
+i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+  if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
+}
+i++;
+}
+return (0);
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* string_toupper - lowercase to uppercase
+* @n: pointer
+* Return: void
+*/
+char *string_toupper(char *n)
+{
+int i;
+i = 0;
+while (n[i] != '\0')
+{
+if (n[i] >= 'a' && n[i] <= 'z')
+n[i] = n[i] - 32;
+i++;
+}
+return (n);
+}
