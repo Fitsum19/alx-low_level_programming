4cbd06db823c706a3791a1d3d81c78d956163018
fitsum19 <fitsumawoke5@gmail.com>
2022-12-22 11:37:37 -0800

    yes

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_number - print numbers
+* @n: integer params
+*/
+void print_number(int n)
+{
+unsigned int n1;
+n1 = n;
+if (n < 0)
+{
+_putchar('-');
+n1 = -n;
+}
+if (n1 / 10 != 0)
+{
+print_number(n1 / 10);
+}
+_putchar((n1 % 10) + '0');
+}
