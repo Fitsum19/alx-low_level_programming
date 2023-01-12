3644983cd6e51763538e6ede6009d726982d209d
fitsum19 <fitsumawoke5@gmail.com>
2023-01-12 11:55:38 -0800

    yes

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* *_memset - fills memory
+* @s: memory area
+* @b: char
+* @n: number
+* Return: pointer
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
+/**
+* *_calloc - allocates memory
+* @nmemb: elements in the array
+* @size: size
+* Return: pointer
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *ptr;
+if (nmemb == 0 || size == 0)
+return (NULL);
+ptr = malloc(size * nmemb);
+if (ptr == NULL)
+return (NULL);
+_memset(ptr, 0, nmemb * size);
+return (ptr);
+}
+
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* *array_range - creates an array
+* @min: minimum range
+* @max: maximum range
+* Return: pointer
+*/
+int *array_range(int min, int max)
+{
+int *ptr;
+int i, size;
+if (min > max)
+return (NULL);
+size = max - min + 1;
+ptr = malloc(sizeof(int) * size);
+if (ptr == NULL)
+return (NULL);
+for (i = 0; min <= max; i++)
+ptr[i] = min++;
+return (ptr);
+}
