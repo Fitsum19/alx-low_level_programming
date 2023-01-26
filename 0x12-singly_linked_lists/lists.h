7ad111db4637989947d26001fed60366dd1b2567
Fitsum19 <fitsumawoke5@gmail.com>
2023-01-26 11:35:57 -0800

    yes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,12 @@
 #ifndef LIST_H
 #define LIST_H
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct lists_s *next;
+}
+list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
