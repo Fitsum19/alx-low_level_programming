78d3b2ce2d28932fb7fcdb05c5556543d91e5349
Fitsum19 <117902393+Fitsum19@users.noreply.github.com>
2023-01-26 22:24:18 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,11 @@
 #ifndef LIST_H
 #define LIST_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-#edif
+#endif
