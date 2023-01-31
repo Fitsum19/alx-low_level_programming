63b9f3c2045cd61b943d5bac7d1b05831a0cf0a7
Fitsum19 <fitsumawoke5@gmail.com>
2023-01-31 01:53:58 -0800

    yes

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,11 @@
  */
 size_t listint_len(const listint_t *h)
 {
- size_t num = 0;
-
- while (h)
- {
- num++;
- h = h->next;
- }
-
- return (num);
+size_t num = 0;
+while (h)
+{
+num++;
+h = h->next;
+}
+return (num);
 }
